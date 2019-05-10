eca2616d922e653e1a342cf87a64330107cff210
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-05-10 16:12:05 +0100

    Set default counts for priority
    This ensures that the various queues are always shown even if the count
    is 0.

--- displayscreen.rb
+++ displayscreen.rb
@@ -35,10 +35,10 @@ get '/' do
   user_protected!
   dark_mode = true unless params[:dark_mode] && params[:dark_mode] == 'false'
   hide_low_queue = true unless params[:hide_low_queue] && params[:hide_low_queue] == 'false'
-  number_of_tickets = {}
+  number_of_tickets = {"high" => 0, "normal" => 0, "low" => 0}
   # This would be much nicer if we could get ticket counts broken down by priority from the Zendesk API
   zendesk.view.find!(id: ENV['ZENDESK_VIEW_ID']).tickets.all! do |ticket|
-    status = ticket.status.fetch("status", "new")
+    status = ticket.fetch("status", "new")
     next if %w(hold pending solved closed).include?(status)
 
     number_of_tickets[ticket['priority']] = number_of_tickets[ticket['priority']].to_i.next
